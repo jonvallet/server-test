a213129be8099af9bffe7ca52d47b855af24921b
jonvallet <j.vallet@gmail.com>
2014-08-14 19:06:29 +0200

    Implement test for same token.

--- src/test/java/com/jonvallet/rest/api/services/DataServiceTest.java
+++ src/test/java/com/jonvallet/rest/api/services/DataServiceTest.java
@@ -9,7 +9,7 @@ public class DataServiceTest {
     @Test
     public void IStoreKeyFirstNameWithValueJonWhenIRetrieveTheValuesAreTheSame(){
 
-        String token = "test-token-id";
+        String token = new TokenService().requestToken();
         String key = "firstName";
         String value = "Jon";
 
@@ -22,7 +22,49 @@ public class DataServiceTest {
     }
 
     @Test
-    public void IStoreTwoValuesICanRetriveBoth(){
+         public void IStoreTwoValuesICanRetriveBoth(){
+
+        String token = new TokenService().requestToken();
+        String key = "firstName";
+        String value = "Jon";
+
+        String token2 = new TokenService().requestToken();
+        String key2 = "firstName";
+        String value2 = "Laura";
+
+        new DataService().saveData(token, key, value);
+        new DataService().saveData(token2, key2, value2);
+
+        String retrieveValue = new DataService().retrieveData(token, key);
+
+        Assert.assertEquals(value, retrieveValue);
+
+        String retrieveValue2 = new DataService().retrieveData(token2, key2);
+
+        Assert.assertEquals(value2, retrieveValue2);
+
+    }
+
+    @Test
+    public void IStoreTwoValuesWithSameTokenAndDifferentKeysICanRetrieveBoth(){
+
+        String token = new TokenService().requestToken();
+        String key = "firstName";
+        String value = "Jon";
+
+        String key2 = "lastName";
+        String value2 = "Vallet";
+
+        new DataService().saveData(token, key, value);
+        new DataService().saveData(token, key2, value2);
+
+        String retrieveValue = new DataService().retrieveData(token, key);
+
+        Assert.assertEquals(value, retrieveValue);
+
+        String retrieveValue2 = new DataService().retrieveData(token, key2);
+
+        Assert.assertEquals(value2, retrieveValue2);
 
     }
 
